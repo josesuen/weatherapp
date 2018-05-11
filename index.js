51ad4d3cd8e4cf260835c99f3655b547cd2f1c85
Thomas <duzevic.thomas@hotmail.com>
2018-05-11 11:00:38 -0300

    Banco Fudido

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { AppRegistry, Text, View } from 'react-native';
 import Main from './src/components/Main';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+
+Amplify.configure(aws_exports);
 
 const App = () => {
   return (
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import NavBar from './NavBar';
+import Amplify, { API } from 'aws-amplify';
 
 class Main extends Component {
-  state = { selectedTab: 0 }
+  state = {
+    selectedTab: 0,
+    apiResponse: null,
+    noteId: ''
+  }
 
-  componentWillMount() {
+  handleChangeNoteId = (event) => {
+   this.setState({noteId: event});
+  }
+
+  async saveNote() {
+    let newNote = {
+      body: {
+        "NoteTitle": "My first note!",
+        "NoteContent": "This is so cool!",
+        "NoteId": "1"
+      }
+    }
+    const path = "/Notes";
+
+    // Use the API module to save the note to the database
+    try {
+      const apiResponse = await API.put("NotesCRUD", path, newNote)
+      console.log("response from saving note: " + apiResponse);
+      this.setState({apiResponse});
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  componentDidMount() {
       this.setState({selectedTab: 1})
   }
 
