178cea7db18cad7eca680bc36e155d375bc72829
José Miguel Suen <jmiguelsuen@gmail.com>
2018-05-19 15:55:28 -0300

    upando imagem pro bucket correto

--- RaspberryPI/upimg.py
+++ RaspberryPI/upimg.py
@@ -1,21 +1,15 @@
-import tinys3
+import boto3
 from picamera import PiCamera
 from time import sleep
 from datetime import date, datetime
 import time
 
+s3=boto3.client('s3')
 camera = PiCamera()
-conn = tinys3.Connection("AKIAIJ6CZ47E4BJYFZDA","xXM3RMdo3b+alGmDkK+LdtVMYX9ohFeMKnW3/r4f")
+tempo = str(time.time())
 camera.start_preview()
 sleep(5)
-tempo = time.time()
-camera.capture('/home/pi/Documents/climatempo/imagens/image%s.jpg' % time)
-f = open('/home/pi/Documents/climatempo/imagens/image%s.jpg' % time,'rb')
-conn.upload('image%s.jpg' % time,f, "dadosestacoes/estacao1")
+filename = tempo + "img.jpg"
+camera.capture('/home/pi/Documents/climatempo/imagens/' + filename)
+s3.upload_file('/home/pi/Documents/climatempo/imagens/' + filename, "dadosestacoes", "estacao1/"+ filename)
 camera.stop_preview()
-
-# Specifying a default bucket
-
-# So we could skip the bucket parameter on every request
-#f = open('cam.jpg','rb')
-#conn.upload('cam.jpg',f, "dadosestacoes/estacao1")
